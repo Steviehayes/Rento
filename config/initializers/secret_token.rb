c0a09d1d36d906a785e1a92ffa7a18007c867005
Steve.j.Hayes <steve.j.hayes@gmail.com>
2011-08-10 14:40:15 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rento::Application.config.secret_token = 'b0e63273aff9e4ecc1032fa33ce8cd1e192418db09305b0fe4181aaa80dc9c798bca799766126633cf1f1042eff7446b3327b2aa924784a7194071b73a7fbec1'
